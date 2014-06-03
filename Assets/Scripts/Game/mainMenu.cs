211bc922bd3cc068932703903b7d449ce736aa8b
Djamali Shawn Jones <15546@ma-web.nl>
2014-06-03 20:35:07 +0200

    Menu gefixt

---
+++ Assets/Scripts/Game/mainMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+//Main menu code. 
+
+public class mainMenu : MonoBehaviour 
+{
+	public Texture backgroundTexture;
+	//button placement kan ook worden aangemaakt voor de x positie. 
+	public float buttonPlacementY; // voor de tweede button. 
+	public float buttonPlacementY2; // voor de derde button
+	public float buttonPlacementY3; // voor de vierde button
+
+	void OnGUI()
+	{
+		//laat de background texture zien. 
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+		//laat de buttons zien. Met de functie om de game scene in te laden.
+		if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "Start Game"))
+		{
+			Application.LoadLevel(1);
+		}
+
+		if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * buttonPlacementY, Screen.width * .5f, Screen.height * .1f), "Instructions"))
+		{
+			
+		}
+
+		if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * buttonPlacementY2, Screen.width * .5f, Screen.height * .1f), "Credits"))
+		{
+			
+		}
+
+		if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * buttonPlacementY3, Screen.width * .5f, Screen.height * .1f), "Quit"))
+		{
+			Application.Quit();
+		}
+
+
+	} 
+
+}
